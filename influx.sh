0401b1143987069030039b6fafc300560d512566
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-12-11 11:34:26 +0100

    send serial output to influxdb

---
+++ influx.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -e
+
+stty -F /dev/ttyUSB0 ispeed 115200 cs8 -cstopb -parenb -echo igncr
+
+cat /dev/ttyUSB0 | while read line ; do
+	curl -XPOST "http://10.13.37.92:8086/write?consistency=any&db=rot13" --data-binary "grove,dc=trnjanska,host=$(hostname -s) $line"
+done
