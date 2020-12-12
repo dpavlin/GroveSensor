40b18decc2373b3f708d2e351f32698bb2ec877e
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-12-12 17:32:30 +0000

    make curl --silent

--- influx.sh
+++ influx.sh
@@ -3,5 +3,5 @@
 stty -F /dev/ttyUSB0 ispeed 115200 cs8 -cstopb -parenb -echo igncr
 
 cat /dev/ttyUSB0 | while read line ; do
-	curl -XPOST "http://10.13.37.92:8086/write?consistency=any&db=rot13" --data-binary "grove,dc=trnjanska,host=$(hostname -s) $line"
+	curl --silent -XPOST "http://10.13.37.92:8086/write?consistency=any&db=rot13" --data-binary "grove,dc=trnjanska,host=$(hostname -s) $line"
 done
