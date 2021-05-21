0697b11a136573edf998b987440a771024ea9ab5
Dobrica Pavlinusic <dpavlin@rot13.org>
2021-05-21 15:15:42 +0000

    change remote ip

--- influx.sh
+++ influx.sh
@@ -1,11 +1,11 @@
-#!/bin/sh -e
+#!/bin/sh
 
 serial=/dev/serial/by-path/platform-xhci-hcd.3.auto-usb-0:2:1.0-port0
 
 stty -F $serial ispeed 115200 cs8 -cstopb -parenb -echo igncr
 
 cat $serial | while read line ; do
-	curl --silent -XPOST "http://10.13.37.92:8086/write?consistency=any&db=rot13" --data-binary "grove,dc=trnjanska,host=$(hostname -s) $line"
+	curl --silent -XPOST "http://10.60.0.92:8086/write?consistency=any&db=rot13" --data-binary "grove,dc=trnjanska,host=$(hostname -s) $line"
 	light=$( echo $line | sed -e 's/^.*,light=//' -e 's/,.*$//' )
 	mosquitto_pub -h rpi2 -t grove/light -m $light
 	echo $line > /dev/shm/grove
